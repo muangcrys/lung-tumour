b8ae35dcd186ca3dc0f86c94467a3d24b8613e82
Jetanat <2923241S@student.gla.ac.uk>
2026-06-17 01:39:46 +0100

    3ch variant for fresh vivit

---
+++ jobs/evaluate_fresh_vivit_3ch.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+model="vivit_random_3ch"
+
+# cd to project root
+cd "/home/s2882278/Diss/lung-tumour"
+echo "###################################"
+echo "#       RUNNING EVALUATION        #"
+echo "###################################"
+
+echo "Finding latest model directory for $model ..."
+parent="/home/s2882278/Diss/lung-tumour/weights/$model"
+latest_dir=$(find "$parent" -mindepth 1 -maxdepth 1 -type d \
+    -printf '%p\n' \
+    | sort \
+    | tail -n1)
+
+echo "Latest model directory: $latest_dir"
+
+# copy files to scratch
+echo "Copying files to scratch..."
+# generate unique id for this run
+UNIQUE_ID=$(date +%Y%m%d%H%M%S)
+WORKING_DIR="eval-$model-$UNIQUE_ID"
+echo "Working directory for this run: $WORKING_DIR"
+bash scripts/copy_files_to_scratch.sh "$WORKING_DIR" > /dev/null
+
+# activate conda
+echo "Activating conda environment..."
+source /opt/conda/bin/activate
+conda activate lung-tumour
+
+# check allocated gpu
+nvidia-smi
+
+# run evaluation script
+python -u src/evaluate_model_dir.py \
+    --model_directory "$latest_dir" \
+    --annotation "/disk/scratch/s2882278/lung-tumour/$WORKING_DIR/data/LUNA/processed/SEED_4242/validate_annotations.csv" \
+    --image_dir "/disk/scratch/s2882278/lung-tumour/$WORKING_DIR/data/LUNA/raw/image" \
+    --preprocessing "vivit_random" \
+    --model_type "vivit" \
+    --channels 1 \
+    --batch_size 8 \
+    --num_workers 2 \
+    --device "cuda"
---
+++ jobs/train_fresh_vivit_3ch.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# cd to project root
+cd "/home/s2882278/Diss/lung-tumour"
+
+# copy files to scratch
+echo "Copying files to scratch..."
+# generate unique id for this run
+UNIQUE_ID=$(date +%Y%m%d%H%M%S)
+WORKING_DIR="vivit-fresh3ch-$UNIQUE_ID"
+echo "Working directory for this run: $WORKING_DIR"
+bash scripts/copy_files_to_scratch.sh "$WORKING_DIR" > /dev/null
+
+# activate conda
+echo "Activating conda environment..."
+source /opt/conda/bin/activate
+conda activate lung-tumour
+
+# check allocated gpu
+nvidia-smi
+
+
+# train model
+echo "Running training script..."
+python -u src/train_fresh_vivit_3ch.py \
+    --train_annotation "/disk/scratch/s2882278/lung-tumour/$WORKING_DIR/data/LUNA/processed/SEED_4242/train_annotations.csv" \
+    --train_image_dir "/disk/scratch/s2882278/lung-tumour/$WORKING_DIR/data/LUNA/raw/image" \
+    --val_annotation "/disk/scratch/s2882278/lung-tumour/$WORKING_DIR/data/LUNA/processed/SEED_4242/validate_annotations.csv" \
+    --val_image_dir "/disk/scratch/s2882278/lung-tumour/$WORKING_DIR/data/LUNA/raw/image" \
+    --num_workers 2 \
+    --report_frequency 5 \
+    --epochs 50 \
+    --batch_size 2 \
+    --learning_rate 5e-5 \
+    --weight_decay 1e-3 \
+    --device "cuda"
+
